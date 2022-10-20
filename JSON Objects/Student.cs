e2897be37bd981f2ae8db1f7489a55f263ce1142
mcdonaldduncan <380939@colum.edu>
2022-10-19 23:20:25 -0500

    update

--- JSON Objects/Student.cs
+++ JSON Objects/Student.cs
@@ -5,30 +5,30 @@ namespace Week4StructuredText.Objects
 {
     internal sealed class Student
     {
-        public string FirstName { get; set; }
+        public string? FirstName { get; set; }
 
-        public string LastName { get; set; }
+        public string? LastName { get; set; }
 
         public bool IsEnrolled { get; set; }
 
         public int YearsEnrolled { get; set; }
 
-        public Address Address1 { get; set; }
+        public Address? Address1 { get; set; }
 
         public Address? Address2 { get; set; }
 
-        public List<PhoneNumber> PhoneNumbers { get; set; }
+        public List<PhoneNumber>? PhoneNumbers { get; set; }
 
         private StringBuilder sb = new StringBuilder();
 
         public override string ToString()
         {
-            sb.Append($"Name: {LastName}, {FirstName}\n");
+            sb.Append($"Name: {LastName ?? "Not available"}, {FirstName ?? "Not available"}\n");
             sb.Append(IsEnrolled ? "Student is currently enrolled.\n" : "Student is not enrolled.\n");
             sb.Append($"Student enrolled for {YearsEnrolled} years\n");
-            sb.Append($"Primary Address: {Address1.ToString()}\n");
-            sb.Append($"Secondary Address: {(Address2 == null ? "No secondary address\n" : $"Secondary Adress: {Address2.ToString()}\n")}");
-            for (int i = 0; i < PhoneNumbers.Count; i++)
+            sb.Append($"Primary Address: {Address1?.ToString() ?? "No primary address"}\n");
+            sb.Append($"Secondary Address: {Address2?.ToString() ?? "No secondary address"}\n");
+            for (int i = 0; i < PhoneNumbers?.Count; i++)
             {
                 sb.Append($"Phone Number {i + 1}: {PhoneNumbers[i].ToString()}");
             }
